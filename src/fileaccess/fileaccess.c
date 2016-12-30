ee8fd089684a74573e28fe166488baf784fc0d49
Andreas Smas <andreas@lonelycoder.com>
2016-12-30 13:46:48 +0100

    Some fixes for extended attributes
    * Make it work on Linux when using the native POSIX filesystem API
    * Improve debugging, in particular tell permissiong denied vs nosupport apart

--- src/fileaccess/fileaccess.c
+++ src/fileaccess/fileaccess.c
@@ -2141,3 +2141,18 @@ far_retain(fa_resolver_t *far)
   atomic_inc(&far->far_refcount);
   return far;
 }
+
+const char *
+fa_err_code_str(fa_err_code_t errcode)
+{
+  switch(errcode) {
+  case FAP_OK: return "OK";
+  case FAP_ERROR: return "Error";
+  case FAP_NEED_AUTH: return "Authentication needed";
+  case FAP_NOT_SUPPORTED: return "Operation not supported";
+  case FAP_PERMISSION_DENIED: return "Permission denied";
+  case FAP_NOENT: return "No such entry";
+  case FAP_EXIST: return "Item already exist";
+  default: return "Unmapped errorcode";
+  }
+}
