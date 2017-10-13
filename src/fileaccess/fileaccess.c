08c0b2562b92c253391bf370bb89ea508148b072
ruralhunter <ruralhunter@gmail.com>
2017-10-13 19:02:23 +0800

    cherry-pick dbaf7713

--- src/fileaccess/fileaccess.c
+++ src/fileaccess/fileaccess.c
@@ -1758,8 +1758,10 @@ fa_load(const char *url, ...)
     return data2;
   }
 
-  if(no_fallback)
+  if(no_fallback) {
+    free(filename);
     return NO_LOAD_METHOD;
+  }
 
   fh = fap->fap_open(fap, filename, errbuf, errlen, 0, 0);
 #ifdef FA_DUMP
